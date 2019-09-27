e609a546a73d7240c220349ae18de6fb3c73d401
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-27 13:28:20 -0700

    entering your name creates an object with your name wins losses ties and current pick of RPS

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -12,19 +12,37 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  var database = firebase.database();
 
 var yourPlayerName = "";
-
+var player1 = null;
+var player2 = null;
 
 
   $("#name-box").on("click", function(event) {
     event.preventDefault();
-    if(yourPlayerName === ""){
+    if(player1 === null){
     yourPlayerName = $("#player-name").val().trim();
     $("#p1Name").text(yourPlayerName)
-    } else {
+    player1 = {
+        name: yourPlayerName,
+        wins: 0,
+        losses: 0,
+        ties: 0,
+        pick: ""
+    };
+    console.log(player1);
+    } else if (player2 === null){
         yourPlayerName = $("#player-name").val().trim();
         $("#p2Name").text(yourPlayerName)
+        player2 = {
+            name: yourPlayerName,
+            wins: 0,
+            losses: 0,
+            ties: 0,
+            pick: ""
+        };
+        console.log(player2);
     }
   });
 
