18583e21c8097289c74d5d933fd5d3285e2a9cd0
Steven Bowman <stevenbowmanjr@gmail.com>
2019-10-03 02:47:55 -0700

    tons of features added, chat works except for the first message got some of the character validation working but it defies logic

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -15,35 +15,111 @@ var firebaseConfig = {
   var database = firebase.database();
 
 var yourPlayerName = "";
-var player1 = null;
-var player2 = null;
 
 
-  $("#name-box").on("click", function(event) {
-    event.preventDefault();
-    if(player1 === null){
+let player1 = {
+  name: "",
+  wins: 0,
+  losses: 0,
+  ties: 0,
+  pick: ""
+};
+database.ref().child("/players/player1").set(player1);
+
+let player2 = {
+  name: "",
+  wins: 0,
+  losses: 0,
+  ties: 0,
+  pick: ""
+};
+database.ref().child("/players/player2").set(player2);
+
+// =====================NAME BUTTON================================
+
+$("#nameButton").on("click", function(event) {
+  event.preventDefault();
+  database.ref("/players").on("value", function(snapshot) {
+    console.log(snapshot.val());
+    console.log(snapshot.val().player1);
+  if(snapshot.val().player1.name == ""){//this has to come from firebase, not local
+    console.log("if condition fired")
     yourPlayerName = $("#player-name").val().trim();
-    $("#p1Name").text(yourPlayerName)
     player1 = {
-        name: yourPlayerName,
-        wins: 0,
-        losses: 0,
-        ties: 0,
-        pick: ""
+      name: yourPlayerName,
+      wins: 0,
+      losses: 0,
+      ties: 0,
+      pick: ""
     };
-    console.log(player1);
-    } else if (player2 === null){
-        yourPlayerName = $("#player-name").val().trim();
-        $("#p2Name").text(yourPlayerName)
-        player2 = {
-            name: yourPlayerName,
-            wins: 0,
-            losses: 0,
-            ties: 0,
-            pick: ""
-        };
-        console.log(player2);
-    }
-  });
+    database.ref().child("/players/player1").set(player1);
+    
+    $("#enter-name").empty();
+    database.ref("/players/player1").onDisconnect().remove();
+    // Though it makes no sense to me removing the ! from player1.name !== "" actually prevents the else if function from firing
+  } else if( (snapshot.val().player1.name == "") && (snapshot.val().player2.name == "")){
+    console.log("if Else function fired")
+    yourPlayerName = $("#player-name").val().trim();
+    $("#p2Name").text(yourPlayerName)
+    player2 = {
+      name: yourPlayerName,
+      wins: 0,
+      losses: 0,
+      ties: 0,
+      pick: ""
+    };
+    database.ref().child("/players/player2").set(player2);
+    $("#enter-name").empty();
+     database.ref("/players/player2").onDisconnect().remove();
+  }//else, 2 people playing already, have to wait. or just chat.
+})});
 
+// ========================Display Players From Firebase======================
+database.ref("/players/player1").on("value", function(snapshot){
+  console.log(snapshot.val());
+  
+  $("#p1Name").text(snapshot.val().name)
+})
+
+
+database.ref("/players/player2").on("value", function(snapshot){
+  console.log(snapshot.val());
+  
+  $("#p2Name").text(snapshot.val().name)
+})
+
+
+
+
+//   ============================== Comment Button===================================
+
+$("#commentButton").on("click", function(event) {
+  event.preventDefault();
+  if ( (yourPlayerName !== "") && ($("#chatInput").val() !== "") ) {
+    var chat = yourPlayerName + ": " + $("#chatInput").val();
+    $("#chatInput").val("");
+    // console.log(chat)
+    // var comment = {
+      //     comment: chat,
+      // }
+      // console.log(comment)
+      
+      database.ref("chat").push(chat);
+      
+      // database.ref().addchild("chat").push(comment);//each message in array has content and sender
+      //db.on("child-added")
+      
+      database.ref("/chat/").on("child_added", function(snapshot){
+        //    console.log(snapshot.val())
+        console.log(snapshot.val())
+        
+        //  $("#chatDisplay").html(`<p>${snapshot.val()}`)
+        var post = snapshot.val();
+        var chatDiv = $("<div>").html(post);
+        $("#chatDisplay").append(chatDiv);
+  $("#chatDisplay").scrollTop($("#chatDisplay")[0].scrollHeight);
+})
+}
+})
+//db.on("value")
 });
